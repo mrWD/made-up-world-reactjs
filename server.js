a7746a86c25ef94bc46bc607fcd95b130222075b
viktor <lvigtor@gmail.com>
2020-08-01 14:20:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const history = require('connect-history-api-fallback');
 
 const app = express();
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/public'));
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
 app.use(staticFileMiddleware);
 app.use(history({
@@ -14,7 +14,7 @@ app.use(history({
 app.use(staticFileMiddleware);
 
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname + '/public/index.html'));
+  res.render(path.join(__dirname + '/dist/index.html'));
 });
 
 const server = app.listen(process.env.PORT || 8080, () => {
